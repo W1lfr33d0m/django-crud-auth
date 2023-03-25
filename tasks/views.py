2fc1443d8e6f68c274463e673ad10633c4f0bc08
W1lfr33d0m <44000106+W1lfr33d0m@users.noreply.github.com>
2023-03-25 14:51:19 -0400

    Django-CRUD-25-03-2023-14-51

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 # Create your views here.
-def helloworld(request):
-    title = 'Hello World'
+def signup(request):
+    
+    if request.method == 'GET':
+        print('Enviando formulario')
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #register User
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password = request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('Passwords do not match')
+    
     return render(request, 'signup.html', {
         'form': UserCreationForm
     })
+
+def home(request):
+    return render(request, 'home.html')
